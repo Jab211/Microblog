ca741ff036ee54a73fa3e5bc0af94cf0e21d7db4
Jason <jasonbalderrama6@gmail.com>
2024-06-26 10:45:19 -0400

    show posts and create posts

--- posts/posts.js
+++ posts/posts.js
@@ -1,3 +1,66 @@
 /* Posts Page JavaScript */
 
 "use strict";
+const postsContainer = document.querySelector("#postsContainer");
+const createPostButton = document.querySelector("#createPostButton");
+const postText = document.querySelector("#postText");
+const loginData = getLoginData();
+
+function loadPosts() {
+  fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${loginData.token}`,
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((posts) => {
+      for (const post of posts) {
+        const postDiv = document.createElement("div");
+        const postHeader = document.createElement("h6");
+  
+        const postCreated = document.createElement("footer");
+        postDiv.classList.add("card");
+        postDiv.classList.add("p-4");
+  
+        postHeader.innerText = post.username;
+        postDiv.innerText = post.text;
+        postCreated.innerText = post.createdAt;
+  
+        postsContainer.appendChild(postHeader);
+        postsContainer.appendChild(postDiv);
+        postsContainer.appendChild(postCreated);
+      }
+    });
+  
+}
+
+//     console.log(posts));
+
+// })
+
+// .then((response) => response.json())
+// .then((userPosts) => {
+
+// // roughly the code needed to create post
+
+function createPost() {
+  fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${loginData.token}`,
+    },
+    body: JSON.stringify({
+      text: postText.value,
+    }),
+  }).then((response) => {
+    loadPosts();
+    return response.json();
+  });
+}
+createPostButton.onclick = createPost;
+
+loadPosts();
