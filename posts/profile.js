fb1206570d1e44144a083624f978e0c79f4da346
Jason <jasonbalderrama6@gmail.com>
2024-06-27 17:51:23 -0400

    display profile info

--- posts/profile.js
+++ posts/profile.js
@@ -1,44 +1,67 @@
 "use strict";
 const logoutButton = document.querySelector("#logoutButton");
+const profileContainer = document.querySelector("#profileContainer");
+const loginData = getLoginData();
 
+console.log(loginData);
 
-
-
-function loadProfile() {
-    fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/users", {
+function getProfile() {
+  fetch(
+    "http://microbloglite.us-east-2.elasticbeanstalk.com/api/users/" +
+      loginData.username,
+    {
       method: "GET",
       headers: {
         Authorization: `Bearer ${loginData.token}`,
       },
+    }
+  )
+    .then((response) => {
+      return response.json();
     })
-      .then((response) => {
-        return response.json();
-      })
-      .then((posts) => {
-        for (const post of posts) {
-          const postDiv = document.createElement("div");
-          const postHeader = document.createElement("h6");
-    
-          const postCreated = document.createElement("footer");
-          postDiv.classList.add("card");
-          postDiv.classList.add("p-4");
-    
-          postHeader.innerText = post.username;
-          postDiv.innerText = post.text;
-          postCreated.innerText = new Date(post.createdAt).toLocaleString();
-    
-          postsContainer.appendChild(postHeader);
-          postsContainer.appendChild(postDiv);
-          postsContainer.appendChild(postCreated);
-        }
-      });
-    
-  }
+    .then((user) => {
+      console.log(user);
+      createProfile(user);
+    });
+}
+function createProfile(user) {
+  const profileDiv = document.createElement("div");
+  profileDiv.classList.add("card");
+  profileDiv.classList.add("p-4");
 
+  const profileHeader = document.createElement("h3");
+  profileHeader.innerText =  `${user.fullName} `;
+  const profileHeader2 = document.createElement("h6");
+  profileHeader2.innerText = `@${user.username}`;
 
+  const profileParagraph = document.createElement("p");
+  profileParagraph.innerText = `Bio: ${user.bio}`;
 
+  profileDiv.appendChild(profileHeader);
+  profileDiv.appendChild(profileHeader2);
+  profileDiv.appendChild(profileParagraph);
 
+  profileContainer.appendChild(profileDiv);
+}
 
+// function createProfile(user) {
+//   const profileDiv = document.createElement("div");
+//   const profileHeader = document.createElement("h6");
+//   const profileParagraph = document.createElement("p");
 
+//   profileDiv.classList.add("card");
+//   profileDiv.classList.add("p-4");
+
+//   profileHeader.innerText = user.fullName;
+//   profileParagraph.innerText = user.username;
+//   profileDiv.innerText = user.bio;
+
+//   profileContainer.appendChild(profileHeader);
+//   profileContainer.appendChild(profileDiv);
+//   profileContainer.appendChild(profileParagraph)
+// }
+
+getProfile();
+// createProfile();
 
 logoutButton.onclick = logout;
